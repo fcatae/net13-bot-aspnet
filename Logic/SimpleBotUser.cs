20dc9bc2e269d286591163d6704b8c26a4e5b062
wagner <wsinfo@msn.com>
2018-09-05 22:06:10 -0300

    inclusao de conexao com o mongoDB

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace SimpleBot
 {
@@ -9,6 +11,19 @@ namespace SimpleBot
     {
         public static string Reply(Message message)
         {
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            var doc = new BsonDocument()
+            {
+                { "id", message.Id},
+                { "texto", message.Text},
+                { "app", "TesteApp"}
+            };
+
+            var db = client.GetDatabase("dbNet13");
+            var col = db.GetCollection<BsonDocument>("user");
+            col.InsertOne(doc);
+
             return $"{message.User} disse '{message.Text}'";
         }
 
