9401cdef0737efcb6a77996436109920d3ff5deb
ronaldomorais <ronaldo.asm@gmail.com>
2018-09-05 22:38:57 -0300

    Get and Set profile

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -28,18 +28,56 @@ namespace SimpleBot
 
             col.InsertOne(doc);
 
+            UserProfile userProfile = GetProfile(message.Id);
+            SetProfile(userProfile.Id, userProfile);
+
             //return $"{message.User} disse '{message.Text}'";
             return returnedText;
         }
 
         public static UserProfile GetProfile(string id)
         {
-            return null;
+            
+            var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("SimpleBotDB");
+            var col = db.GetCollection<BsonDocument>("UserProfile");
+
+            var filtro = Builders<BsonDocument>.Filter.Eq("id", id);
+            var res = col.Find(filtro).ToList();
+
+            UserProfile userProfile = new UserProfile();
+            if (res.Count == 0)
+            {
+                userProfile.Id = id;
+                userProfile.Visitas = 1;
+            }
+            else
+            {
+            }
+
+            return userProfile;
         }
 
         public static void SetProfile(string id, UserProfile profile)
         {
+            var doc = new BsonDocument
+            {
+                {"Id", profile.Id },
+                {"Visitas", profile.Visitas }
+            };
 
+            var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("SimpleBotDB");
+            var col = db.GetCollection<BsonDocument>("UserProfile");
+
+            if (profile.Visitas == 1)
+            {
+                col.InsertOne(doc);
+            }
+            else
+            {
+                col.ReplaceOne(doc, doc);
+            }
         }
     }
 }
