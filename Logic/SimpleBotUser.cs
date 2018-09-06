bbf89f13ffd1fd39547b53d9307796ce7434a5b7
Everton Silvestre <everton.srs@gmail.com>
2018-09-05 22:44:57 -0300

    Desafio 02

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -2,23 +2,89 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace SimpleBot
 {
     public class SimpleBotUser
     {
+
+        public static string Popula_UserProfile(Message message)
+        {
+            var id = message.Id;
+
+            var prof = GetProfile(id);
+
+            prof.Visitas += 1;
+
+            SetProfile(id, prof);
+
+            return $"{message.User} disse '{message.Text} e mandou {prof.Visitas} '";
+        }
+        
         public static string Reply(Message message)
         {
+
+            Popula_UserProfile(message);
+
+            var client = new MongoClient("mongodb://localhost:27017");
+
+
+            var db = client.GetDatabase("db01");
+            var col = db.GetCollection<BsonDocument>("tabela01");
+            
+            var doc = new BsonDocument()
+            {
+                {"id",message.Id },
+                {"texto", message.Text },
+                {"app","teste2" }
+                
+            };         
+
+            col.InsertOne(doc);
+           
             return $"{message.User} disse '{message.Text}'";
         }
 
         public static UserProfile GetProfile(string id)
-        {
-            return null;
+        {        
+
+
+            var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("db01");
+            var col = db.GetCollection<UserProfile>("Profile01");
+
+
+            var filtro = Builders<UserProfile>.Filter.Eq("id",id);
+
+            var profile = col.Find(filtro).FirstOrDefault(); 
+
+            if (profile == null)
+            {
+                return new UserProfile()
+                {
+                    Id = id,
+                    Visitas = 0
+                };
+
+            }
+
+
+            return profile;
         }
 
         public static void SetProfile(string id, UserProfile profile)
         {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("db01");
+            var col = db.GetCollection<UserProfile>("Profile01");
+
+            var filtro = Builders<UserProfile>.Filter.Eq("id", id);
+
+            col.ReplaceOne(filtro, profile);
+
         }
     }
 }
