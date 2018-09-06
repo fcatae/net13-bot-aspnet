5a31c029012cd17809d0a3295fb4bb7f213eefb7
carlaNobrega <carlanobrega22@gmail.com>
2018-09-05 22:45:18 -0300

    Implementando GetProfile

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -9,33 +9,67 @@ namespace SimpleBot
 {
     public class SimpleBotUser
     {
+        
         public static string Reply(Message message)
         {
-            var client = new MongoClient("mongodb://localhost:27017");
+            var id = message.Id;
+            var profile = GetProfile(id);
+            profile.Visitas += 1;
 
-            var doc = new BsonDocument
-            {
-                { "id", message.Id},
-                { "texto", message.Text},
-                { "app", "teste"}
-            };
+            SetProfile(id, profile);
 
-            var db = client.GetDatabase("db01");
-            var col = db.GetCollection<BsonDocument>("tabela01");
+            //var client = new MongoClient("mongodb://localhost:27017");
 
-            col.InsertOne(doc);
+            //var doc = new BsonDocument
+            //{
+            //    { "id", message.Id},
+            //    { "texto", message.Text},
+            //    { "app", "teste"}
+            //};
 
+            //var db = client.GetDatabase("db01");
+            //var col = db.GetCollection<BsonDocument>("tabela01");
+
+            //col.InsertOne(doc);
 
             return $"{message.User} disse '{message.Text}'";
         }
 
         public static UserProfile GetProfile(string id)
         {
-            return null;
+            var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("db01");
+            var col = db.GetCollection<BsonDocument>("tabela01");
+
+            var filtro = Builders<BsonDocument>.Filter.Eq("id", id);
+            var res = col.Find(filtro).ToList();
+
+            if (res.Count == 0)
+            {
+                return new UserProfile
+                {
+                    Id = id,
+                    Visitas = 0
+                };
+
+            }
+            else {
+                //retornar o documento;
+                return null;
+            }
+            
         }
 
         public static void SetProfile(string id, UserProfile profile)
         {
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("db01");
+            var col = db.GetCollection<BsonDocument>("userProfile");
+
+            var filtro = Builders<BsonDocument>.Filter.Gt("_id", 1);
+
+
         }
     }
 }
