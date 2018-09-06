c964f8f2b371c1e4f0577d559a01d82f9fa1c804
Luiz <luiz.fernando.desenvolvimento@outlook.com>
2018-09-05 22:38:54 -0300

    mensagens

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,16 +11,49 @@ namespace SimpleBot
     {
         public static string Reply(Message message)
         {
-            return $"{message.User} disse '{message.Text}'";
+            //MongoClient client = new MongoClient("mongodb://localhost:27017");
+            //var db = client.GetDatabase("User");
+            //var col = db.GetCollection<BsonDocument>("Messages");
+
+            //var ms = new BsonDocument {
+            //    { "id", message.Id},
+            //    { "mensagem", message.Text},
+            //    { "app","conn"}
+            //};
+            //col.InsertOne(new Pessoa()
+            //{
+            //    FirstName = "Dona",
+            //    LastName = "Maria",
+            //    Idade = 50,
+            //    Email = "donamaria@outlook.com.br"
+            //});
+            //col.InsertOne(ms);
+            var ret = GetProfile(message.Id);
+            int total = ret == null ? 0 : ret.Visitas;
+            SetProfile(message.Id, new UserProfile() { Id = message.Id, Visitas = ++total });
+
+            ret = GetProfile(message.Id);
+            return $"{message.User} disse '{message.Text}', total de Visitas: '{ret.Visitas}'";
         }
 
         public static UserProfile GetProfile(string id)
         {
-            return null;
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("User");
+            var col = db.GetCollection<UserProfile>("UserProfile");
+            var filtro = Builders<UserProfile>.Filter.Eq("_id", id);
+            return col.Find(filtro).FirstOrDefault();
+
         }
 
         public static void SetProfile(string id, UserProfile profile)
         {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            IMongoDatabase db = client.GetDatabase("User");
+            IMongoCollection<UserProfile> col = db.GetCollection<UserProfile>("UserProfile");
+            FilterDefinition<UserProfile> filtro = Builders<UserProfile>.Filter.Eq("_id", id);
+            col.ReplaceOne(filtro, profile);
+
         }
     }
 }
