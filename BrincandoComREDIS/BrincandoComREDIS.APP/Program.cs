b795dd973ab33659a317dd7e8c8fe50a677123be
RodrigoFermiano <rc.fermiano@hotmail.com>
2018-09-12 22:48:45 -0300

    REDIS

---
+++ BrincandoComREDIS/BrincandoComREDIS.APP/Program.cs
@@ -0,0 +1,30 @@
+﻿using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrincandoComREDIS.APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apiKey = "AIzaSyCVGlnfsGaaJ3HYTHVu6OH_jBaIFJQYrH0";
+            string cx = "013638634553566051485:xwnfxqp2pso";
+            string query = "3 + 2?";
+            var svc = new Google.Apis.Customsearch.v1.CustomsearchService(
+            new BaseClientService.Initializer { ApiKey = apiKey });
+            var listRequest = svc.Cse.List(query);
+            listRequest.Cx = cx;
+            var search = listRequest.Execute(); //listRequest.Fetch(); This method has been removed
+            foreach (var result in search.Items)
+            {
+                Console.WriteLine("Title: {0}", result.Title.FirstOrDefault());
+                Console.WriteLine("Link: {0}\n", result.Link.FirstOrDefault());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BrincandoComREDIS/BrincandoComREDIS.Dados/REDIS_DB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace BrincandoComREDIS.Dados
+{
+    public class REDIS_DB
+    {
+        public ConnectionMultiplexer redis { get; set; }
+        public ISubscriber sub { get; set; }
+
+        public void Connect(string channel)
+        {
+            redis = ConnectionMultiplexer.Connect(channel);
+            IDatabase db = redis.GetDatabase();
+        }
+        public string Subscriber(string channel)
+        {
+
+            string redisValues = string.Empty;
+            sub = redis.GetSubscriber();
+            sub.Subscribe(channel, (ch, msg) =>
+            {
+                redisValues = msg.ToString();
+            });
+
+
+            return redisValues;
+        }
+
+        public long Publisher(string channel, string message)
+        {
+            var sub = redis.GetSubscriber();
+            long retorno = sub.Publish(channel, message);
+            return retorno;
+        }
+
+        public string HSet()
+        {
+            return string.Empty;
+        }
+    }
+}
