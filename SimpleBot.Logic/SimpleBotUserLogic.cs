999a7b4bf08fbfeec3c5dba53cd10b7eccff7af2
Fabricio Catae <fcatae@users.noreply.github.com>
2018-09-22 20:18:01 -0300

    Update SimpleBotUserLogic.cs

--- SimpleBot.Logic/SimpleBotUserLogic.cs
+++ SimpleBot.Logic/SimpleBotUserLogic.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 
 namespace SimpleBot.Logic 
 {
+    // Parabens!!! Retiraram os statics!!! Isso era um erro do meu codigo..
+    
     public class SimpleBotUserLogic 
     {   
 
