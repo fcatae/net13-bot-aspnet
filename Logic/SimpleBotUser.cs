a1162f171ae05d3f9396d4d9fc2c1736a6e5a755
Fabricio Catae <fcatae@users.noreply.github.com>
2018-09-22 21:49:05 -0300

    Update SimpleBotUser.cs

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -2,13 +2,27 @@
 
 namespace SimpleBot.Logic
 {
+    // Parabens! Retiraram o "static" da classe
+    
     public  class SimpleBotUser
     {
+        // sugestao: use o construtor para receber o Repositorio
+        IRepository _repo;        
+        public SimpleBotUser(IRepository repo)
+        {
+            this._repo = repo;
+        }        
+        
+        // implementacao incorreta do Pattern
+        
         public string Popula_UserProfile(Message message)
         {
             var id = message.Id;
             UserProfile prof;
 
+            // Nao use a classe Contexto
+            // Nao use a classe UserProfileSqlRepository
+            // Use somente a propriedade _repo, definida anteriormente
             using (var context = new Contexto())
             {
                 var userProfileSql = new UserProfileSqlRepository(context);
